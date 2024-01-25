6adcf462eecf4ea0fe1c003a52d2d036e120478c
Maxim Popov <maximpopovdev@gmail.com>
2024-01-25 07:23:30 +0200

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,223 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+void MainWindow::on_pushButtonFindDrivers_clicked()
+{
+    QList<QStorageInfo> storageList = QStorageInfo::mountedVolumes(); // list of drivers
+    ui->comboBoxDrivers->clear();
+    foreach (const QStorageInfo &storage, storageList) { // add all drivers
+        ui->comboBoxDrivers->addItem(storage.rootPath());
+    }
+
+    for (int i = 0; i < 3; i++) // delete MY drivers
+    {
+        ui->comboBoxDrivers->removeItem(0);
+    }
+}
+
+
+
+void MainWindow::on_pushButtonOpenFile_clicked()
+{
+    //QString openFilePath = ui->lineEditOpenFileLocation->text();
+
+    QList<QStorageInfo> storageList = QStorageInfo::mountedVolumes(); // list of drivers
+
+    QString selectedStorageName = ui->comboBoxDrivers->currentText(); // get current driver letter
+
+    QStorageInfo selectedStorage;
+    foreach (const QStorageInfo &storage, storageList) { // set current driver because its letter
+        if (storage.rootPath() == selectedStorageName) {
+            selectedStorage = storage;
+            break;
+        }
+    }
+
+    // set driver information in labels
+    ui->labelName->setText(selectedStorage.name());
+    long long memoryFree = selectedStorage.bytesFree() / (1024 * 1024);
+    ui->labelFreeSpace->setText(QString::number(memoryFree) + " MB");
+    long long memoryTotal = selectedStorage.bytesTotal() / (1024 * 1024);
+    ui->labelCapacity->setText(QString::number(memoryTotal) + " MB");
+    ui->labelType->setText(selectedStorage.fileSystemType());
+    ui->labelAddress->setText(selectedStorage.rootPath());
+    ui->labelBaudRate->setText("9.5 Mbit/s");
+
+    // set driver letter in file locations
+    ui->lineEditOpenFileLocation->setText(selectedStorageName);
+    ui->lineEditSaveFileLocation->setText(selectedStorageName);
+}
+
+
+QString fileContent;
+void MainWindow::on_pushButtonShowData_clicked()
+{
+    QString filePath = ui->lineEditOpenFileLocation->text(); // get file path from label
+    QFile file(filePath); // create object file
+
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) { // try to open file
+        if(file.size() == 0) { // if file open but it is empty
+            ui->lineEditOutputData->setStyleSheet("color: rgb(255, 0, 0);background-color: rgb(255, 255, 255);");
+            ui->lineEditOutputData->setText("ERROR! FILE IS EMPTY!");
+        } else { // if file open
+            ui->lineEditOutputData->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);"); // return black color for text after misstake
+            QTextStream in(&file); // create text reader from file
+
+            in.setCodec("UTF-8"); // utf-8
+
+            fileContent = in.readAll(); // read all data from file
+            //fileData = in.readAll(); // global data for NextSymbol
+
+            ui->lineEditOutputData->setText(fileContent); // set data to label
+
+            ui->labelSymbol->setText(fileContent.at(0)); // set first letter to label
+
+            QTextCodec *codec = QTextCodec::codecForName("CP866"); // codec
+            QByteArray byteArray = codec->fromUnicode(fileContent); // byte array from letter codes mb
+            int byte = byteArray.at(0); // get first element
+            QString ascii = QString("%1").arg(QString::number(byte, 2), 8, '0').right(8); // make ascii 8bit code
+            ui->labelASCII->setText(ascii); // set ascii code to label
+
+            QString nrzi;
+            if (ascii.at(0) == "1") { // if first ascii num == 1
+                int temp = 0; // set temp 0
+                nrzi = "0"; // set first nrzi num 0
+                for (int i = 1; i < 8; i++) { // for other 7 bits
+                    if(ascii.at(i) == "1") { // if ascii num == 1
+                        nrzi += QString::number(temp); // set nrzi num same to previous
+                    } else { // else if ascii num == 0
+                        if (temp == 0) // invert temp
+                            temp++;
+                        else
+                            temp--;
+                        nrzi += QString::number(temp); // and set it
+                    }
+                }
+            } else { // else if first ascii num == 1
+                int temp = 1; // set temp 1
+                nrzi = "1"; // set first nrzi num 1
+                for (int i = 1; i < 8; i++) { // for other 7 bits
+                    if(ascii.at(i) == "1") { // if ascii num == 1
+                        nrzi += QString::number(temp); // set nrzi num same to previous
+                    } else { // else if ascii num == 0
+                        if (temp == 0) // invert temp
+                            temp++;
+                        else
+                            temp--;
+                        nrzi += QString::number(temp); // and set it
+                    }
+                }
+            }
+
+            ui->labelNRZI->setText(nrzi); // set nrzi codeto label
+
+            file.close(); // close file
+        }
+        } else { // else if path to file is wrong
+            ui->lineEditOutputData->setStyleSheet("color: rgb(255, 0, 0);background-color: rgb(255, 255, 255);");
+            ui->lineEditOutputData->setText("ERROR! FILE IS NOT FOUND!");
+        }
+}
+
+int currentLetter = 0;
+void MainWindow::on_pushButtonNextSymbol_clicked()
+{
+    currentLetter++;
+    if (currentLetter == fileContent.length()) {
+        currentLetter = 0;
+    }
+
+
+
+    ui->labelSymbol->setText(fileContent.at(currentLetter)); // set first letter to label
+
+    QTextCodec *codec = QTextCodec::codecForName("CP866"); // codec
+    QByteArray byteArray = codec->fromUnicode(fileContent); // byte array from letter codes mb
+    int byte = byteArray.at(currentLetter); // get current element
+    QString ascii = QString("%1").arg(QString::number(byte, 2), 8, '0').right(8); // make ascii 8bit code
+    ui->labelASCII->setText(ascii); // set ascii code to label
+
+    QString nrzi;
+    if (ascii.at(0) == "1") { // if first ascii num == 1
+        int temp = 0; // set temp 0
+        nrzi = "0"; // set first nrzi num 0
+        for (int i = 1; i < 8; i++) { // for other 7 bits
+            if(ascii.at(i) == "1") { // if ascii num == 1
+                nrzi += QString::number(temp); // set nrzi num same to previous
+            } else { // else if ascii num == 0
+                if (temp == 0) // invert temp
+                    temp++;
+                else
+                    temp--;
+                nrzi += QString::number(temp); // and set it
+            }
+        }
+    } else { // else if first ascii num == 1
+        int temp = 1; // set temp 1
+        nrzi = "1"; // set first nrzi num 1
+        for (int i = 1; i < 8; i++) { // for other 7 bits
+            if(ascii.at(i) == "1") { // if ascii num == 1
+                nrzi += QString::number(temp); // set nrzi num same to previous
+            } else { // else if ascii num == 0
+                if (temp == 0) // invert temp
+                    temp++;
+                else
+                    temp--;
+                nrzi += QString::number(temp); // and set it
+            }
+        }
+    }
+
+    ui->labelNRZI->setText(nrzi); // set nrzi codeto label
+}
+
+void MainWindow::on_pushButtonSaveFile_clicked()
+{
+    QString dataString = ui->lineEditOutputData->text(); // data
+    QString filePath = ui->lineEditSaveFileLocation->text(); // save data in file
+
+    QFile file(filePath); // create object file
+
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) { // try to open file
+        ui->lineEditOutputData->setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);"); // return black color for text after misstake
+        QTextStream stream(&file);
+        stream.setCodec("UTF-8"); // utf-8
+        stream << dataString; // write data to file
+        file.close();
+    } else { // else if file is not found
+        ui->lineEditOutputData->setStyleSheet("color: rgb(255, 0, 0);background-color: rgb(255, 255, 255);");
+        ui->lineEditOutputData->setText("ERROR! FILE IS NOT FOUND!");
+    }
+
+    // clear all widgets
+    ui->comboBoxDrivers->clear();
+
+    ui->lineEditOpenFileLocation->clear();
+    ui->lineEditSaveFileLocation->clear();
+    ui->lineEditOutputData->clear();
+
+    ui->labelName->clear();
+    ui->labelFreeSpace->clear();
+    ui->labelCapacity->clear();
+    ui->labelType->clear();
+    ui->labelAddress->clear();
+    ui->labelBaudRate->clear();
+
+    ui->labelSymbol->clear();
+    ui->labelASCII->clear();
+    ui->labelNRZI->clear();
+}
---
+++ mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QStorageInfo>
+#include <QFile>
+#include <QTextStream>
+#include <QTextCodec>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButtonFindDrivers_clicked();
+
+    void on_pushButtonOpenFile_clicked();
+
+    void on_pushButtonShowData_clicked();
+
+    void on_pushButtonNextSymbol_clicked();
+
+    void on_pushButtonSaveFile_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
